86e58fe9e77dc97c5955bf80581371e41b1e8096
Sergio <sergioscarpin@gmail.com>
2022-04-30 11:00:42 -0300

    add usuario hardcode

--- src/com/company/SingletonBD.java
+++ src/com/company/SingletonBD.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 
 public class SingletonBD {
 
-    private static ArrayList<Usuario> listaUsuario = new ArrayList<Usuario>();
-
     private SingletonBD() { }
 
-    public static void adicionarUsuario(){ }
+    public static void adicionarUsuario(){
+        Usuario usuario1 = new Usuario("joao", "joao", "João Souza", 1992);
+        Usuario usuario2 = new Usuario("maria", "maria", "Maria Silva", 1995);
+
+
+
+    }
 
     public static void deletarUsuario(){ }
 
