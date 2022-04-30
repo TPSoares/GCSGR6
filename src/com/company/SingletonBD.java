08909a500f2e6a673727c49e9fce15b07df4d75c
Sergio <sergioscarpin@gmail.com>
2022-04-30 11:10:40 -0300

    add usuario

--- src/com/company/SingletonBD.java
+++ src/com/company/SingletonBD.java
@@ -1,16 +1,23 @@
 package com.company;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SingletonBD {
 
     private SingletonBD() { }
 
     public static void adicionarUsuario(){
-        Usuario usuario1 = new Usuario("joao", "joao", "João Souza", 1992);
-        Usuario usuario2 = new Usuario("maria", "maria", "Maria Silva", 1995);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o nome do usuário: ");
+        String name = sc.next();
+        System.out.println("Informe a senha do usuário: ");
+        String senha = sc.next();
+        System.out.println("Informe o ano de nascimento do usuário: ");
+        Integer anoNascimento = sc.nextInt();
 
+        Usuario usuario = new Usuario("Usuário", senha, name, anoNascimento);
 
+        //usuario.setUsuario();
 
     }
 
