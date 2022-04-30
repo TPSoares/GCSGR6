0d7fff2314411ff59dcede9bd94ee6c0b3bbc00f
Victor Leal <vcleal@gmail.com>
2022-04-30 11:23:56 -0300

    codigo para atualizar usuario

--- src/com/company/SingletonBD.java
+++ src/com/company/SingletonBD.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SingletonBD {
 
@@ -12,7 +13,31 @@ public class SingletonBD {
 
     public static void deletarUsuario(){ }
 
-    public static void atualizarUsuario(){ }
+    public static void atualizarUsuario() {
+        boolean found = false;
+        Scanner sc = new Scanner(System.in);
+        String user, nome, senha;
+        int anoNascimento;
+        System.out.print("Usuário para atualizar: ");
+        user = sc.nextLine();
+        for (Usuario usuario : listaUsuario) {
+            if (usuario.getUsuario().equals(user)) {
+                found = true;
+                System.out.print("Atualizar nome: ");
+                nome = sc.nextLine();
+                usuario.setNome(nome);
+                System.out.print("Atualizar data de nascimento: ");
+                anoNascimento = sc.nextInt();
+                usuario.setAnoNascimento(anoNascimento);
+                System.out.print("Atualizar senha: ");
+                senha = sc.nextLine();
+                usuario.setSenha(senha);
+                break;
+            }
+        }
+        if (!found)
+            System.out.println("Usuário não encontrado");
+    }
 
     public static void listarUsuarios(){ }
 }
